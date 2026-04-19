d4f9431ad045d8fbcf8f3daad1947129e4c6a823
José Aguilera <jose@aguilera.dev>
2026-04-19 21:47:25 +0200

    DaysUntilExpiration() tests

--- date_test.go
+++ date_test.go
@@ -9,7 +9,21 @@ import (
 func TestExpireDate(t *testing.T) {
 	assert := assert.New(t)
 
-	f, _ := FromString(`Expires: 2026-01-31T22:00:00.000Z`)
+	// Missing value
+	f, _ := FromString(`Contact: example@example.com`)
+
+	assert.Nil(f.Expiration())
+	assert.Equal(f.DaysUntilExpiration(), 0)
+	assert.False(f.Expired())
+
+	// Empty value
+	f, _ = FromString(`Expires:`)
+
+	assert.Nil(f.Expiration())
+	assert.False(f.Expired())
+
+	// Valid value
+	f, _ = FromString(`Expires: 2026-01-31T22:00:00.000Z`)
 
 	assert.NotNil(f.Expiration())
 	assert.Equal(f.Expiration().Year(), 2026)
